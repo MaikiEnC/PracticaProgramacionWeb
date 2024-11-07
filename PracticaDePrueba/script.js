3a7920e0c732ce75bf5d1e193a044a6c82536575
mikiC# <2022030025@upsin.edu.mx>
2024-11-07 07:45:58 -0700

    Programacion Web Practica Base de Datos Firebase
    Usando Bootstrap se diseño este producto
    Firebase como base de Datos

---
+++ PracticaDePrueba/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",domReady);
+
+function domReady(){
+    mostrarAsync();
+}
+
+async function agregarDatos(){
+    const url = 'https://upsinpractica-default-rtdb.firebaseio.com/';
+
+
+}
